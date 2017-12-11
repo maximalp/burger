e89c13af05a4db923d9c67a0e63eb1a5c3eb658f
Maxim Lopatnikov <maxim.alp@gmail.com>
2017-12-10 21:54:19 -0500

    app crash fix

--- server.js
+++ server.js
@@ -22,4 +22,8 @@ var routes = require("./controllers/burgers_Controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+//app.listen(port);
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
